2a7c2c8b34b57a4696ab92de2138e336781a1f02
Michal Szukala <szukalagithub@gmail.com>
2018-01-04 21:38:01 +0100

    GUI buttons actions

--- src/com/git/michalszukala/christmasgifts/People.java
+++ src/com/git/michalszukala/christmasgifts/People.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-//Class contains data of every person we want send gifts.  It's performing a gifts draw as well.
+//Class contains data of every person we want send a gifts.  It's performing as well draw of gifts.
 public class People {
 
     private String name = "NAME";
     private String phone ="PHONE";
     private String email = "EMAIL";
     private List<People> peopleList = new ArrayList<>();
-    private List<String> luckyCouplesArray = new ArrayList<>();
+    private List<String> luckyCouplesArray;
 
 
 
@@ -48,11 +48,35 @@ public class People {
             peopleList.add(person);
     }
 
+    //returns value of ArrayList with draw's Results
+    public String getDrawResultAtIndex(int index){
+        int size = luckyCouplesArray.size();
+
+        if(index > -1 && index < size)
+            return luckyCouplesArray.get(index);
+        else
+            return "Index Error";
+    }
+
+    //removes one person from peopleList
+    public void removeFromPeopleList(People person){
+        peopleList.remove(person);
+    }
+
+
+    public int getSizeDrawResults(){
+        if(luckyCouplesArray.size() > -1)
+            return luckyCouplesArray.size();
+        else
+            return -1;
+    }
+
 
     //Takes first person from the list and draws random second person to it.  Sends both of them emails for whom they should buy gifts
     //then removes person's details from the list,and draws again until there is no or one person left on the list
     public void drawGifts(){
         List<People> drawList = new ArrayList<>();
+        luckyCouplesArray = new ArrayList<>();
         String luckyCouple;
 
         for(People x : peopleList)
--- src/com/git/michalszukala/christmasgifts/PeopleListController.java
+++ src/com/git/michalszukala/christmasgifts/PeopleListController.java
@@ -1,17 +1,112 @@
 package com.git.michalszukala.christmasgifts;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PeopleListController implements Initializable {
 
+    private ObservableList<People> listOfPeople;
+    private People people;
 
+
+    @FXML private TableView<People> tableOfPeople;
+    @FXML private TableColumn<People, String> tableColumnName;
+    @FXML private TableColumn<People, String> tableColumnPhone;
+    @FXML private TableColumn<People, String> tableColumnEmail;
+    @FXML private TextField textFieldName;
+    @FXML private TextField textFieldPhone;
+    @FXML private TextField textFieldEmail;
+
+    // even handler for Add button, it is adding a new person
+    @FXML
+    public void addButton(ActionEvent event) {
+
+        if (dataValidation()) {
+            People addNewPerson = new People();
+
+            addNewPerson.setName(textFieldName.getText());
+            addNewPerson.setPhone(textFieldPhone.getText());
+            addNewPerson.setEmail(textFieldEmail.getText());
+
+            listOfPeople.add(addNewPerson);
+            people.setPerson(addNewPerson);
+
+            textFieldName.clear();
+            textFieldPhone.clear();
+            textFieldEmail.clear();
+        }
+    }
+
+    // even handler for Delete button, it is deleting a person from the table
+    @FXML
+    public void deleteButton(){
+        ObservableList<People> selectedRow = tableOfPeople.getSelectionModel().getSelectedItems();
+        People person = tableOfPeople.getSelectionModel().getSelectedItem();
+        listOfPeople.removeAll(selectedRow);
+        people.removeFromPeopleList(person);
+    }
+
+    // even handler for Draw button, it is calling method responsible for drawing gifts from the People class
+    @FXML
+    public void drawButton(ActionEvent event) {
+        if(listOfPeople.size() != 0) {
+            people.drawGifts();
+            drawConfirmationWindow();
+        }
+        else{
+            alertWindow("You have nobody on the present's list!!");
+        }
+    }
+
+    //data validation
+    private boolean dataValidation() {
+        boolean test = true;
+
+
+        return test;
+    }
+
+
+
+    //window with messages
+    private void alertWindow(String message){
+
+    }
+
+    //window with draw results
+    private void drawResultsWindow(){
+
+    }
+
+    //confirmation window if user want to see draw results
+    private void drawConfirmationWindow(){
+
+    }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<People, String>("name"));
+        tableColumnPhone.setCellValueFactory(new PropertyValueFactory<People, String>("phone"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<People, String>("email"));
+
+
+        listOfPeople = FXCollections.observableArrayList();
+        people = new People();
+
+        tableOfPeople.setItems(listOfPeople);
+
+
 
 
     }
